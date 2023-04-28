82253751dfbd3eaf6b9fd8cd6fe5e9f95111ada1
timosha <vorobey6b6@gmail.com>
2023-04-28 13:48:51 +0300

    update parsing and routing

--- main/main/urls.py
+++ main/main/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from rest.views import TestApiView
+from rest.views import documentToMindmap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/test/', TestApiView.as_view())
+    path('api/test/', documentToMindmap.as_view())
 ]
--- main/rest/views.py
+++ main/rest/views.py
@@ -1,26 +1,48 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
 import docx
 
 
 # Create your views here.
-class TestApiView(APIView):
+class documentToMindmap(APIView):
     def get(self, request):
-        doc = docx.Document('static/data/docs.docx')
-
-        text = []
-        headers = []
-        textForHeader = []
+        doc = docx.Document('static/data/example.docx')
+        result = {}
+        current_header = ''
+        current_subheader = ''
+        current_text = ''
         for paragraph in doc.paragraphs:
-            if paragraph.text[:1] == '#':
-                headers.append(paragraph.text[1:])
-                if textForHeader:
-                    text.append(textForHeader)
-                    textForHeader = []
-                continue
+            text = paragraph.text.strip()
+            if text.startswith('##'):
+                if current_header:
+                    if current_subheader and current_subheader == text.lstrip('#').strip():
+                        # Если у нас уже есть subheader с таким же названием, то не добавляем его
+                        continue
+                    result[current_header].append(
+                        {'subheader': current_subheader.strip(), 'text': current_text.strip()})
+                    current_text = ''
+                current_subheader = text.lstrip('#').strip()
+            elif text.startswith('#'):
+                if current_header:
+                    result[current_header].append(
+                        {'subheader': current_subheader.strip(), 'text': current_text.strip()})
+                current_header = text.lstrip('#').strip()
+                current_subheader = ''
+                current_text = ''
+                result[current_header] = []
+            else:
+                current_text += text + ' '
 
-            textForHeader.append(paragraph.text)
+        if current_header:
+            result[current_header].append({'subheader': current_subheader.strip(), 'text': current_text.strip()})
 
-        text.append(textForHeader)
+        for headers in result.values():
+            unique_subheaders = []
+            for subheader in headers:
+                if subheader['subheader'] not in unique_subheaders:
+                    unique_subheaders.append(subheader['subheader'])
+                else:
+                    headers.remove(subheader)
 
-        return Response([headers, text])
+        return Response(json.dumps(result, indent=4))
